0172afe3e3c24da5d6a551260c36d8a92fe723cf
Juan Gabriel Rios D'Amico <juangrios27@gmail.com>
2023-03-02 23:58:53 -0300

    Feat - Maqueta terminos de uso y politica de privacidad

--- frontend/src/app/modules/user/containers/settings/settings.component.ts
+++ frontend/src/app/modules/user/containers/settings/settings.component.ts
@@ -7,9 +7,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent {
-  constructor(private _route: Router) {}
+  terms: boolean;
+  policity: boolean;
+  constructor(private _route: Router) {
+    this.terms = false;
+    this.policity = false;
+  }
   logOut() {
     localStorage.clear();
     this._route.navigate(['auth']);
+  }
+
+  toogleModaltermsOfUse() {
+    this.terms = !this.terms;
+  }
+  toogleModalPolicity() {
+    this.policity = !this.policity;
   }
 }
